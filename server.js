6fa7d8500181c1e7b33e0fb0c0a51a637cd213a3
stacysn <stacysuen@Stacy-Staces-MacBook-Pro.local>
2017-07-20 11:43:59 -0700

    created route to respond with GET

--- server.js
+++ server.js
@@ -3,3 +3,9 @@ var express = require {'express'}
 
 var bodyParse = require('body-parser');
 app.use(bodyParser.urlencoded[{extended: true}]);
+
+//ROUTES!
+
+app.get("/", function (req, res){
+  res.send("Hello thereeeee ;) ")
+})
