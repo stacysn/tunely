584dc41c2e79511da9b18555c35747e2c33260f0
stacysn <stacysuen@Stacy-Staces-MacBook-Pro.local>
2017-07-21 12:05:00 -0700

    added forEach to add songs in albumList

--- seed.js
+++ seed.js
@@ -5,29 +5,57 @@ var db = require("./models");
 
 var albumList =[];
 albumList.push({
-              artistName: 'Nine Inch Nails',
-              name: 'The Downward Spiral',
-              releaseDate: '1994, March 8',
-              genres: [ 'industrial', 'industrial metal' ]
-            });
+  artistName: 'Nine Inch Nails',
+  name: 'The Downward Spiral',
+  releaseDate: '1994, March 8',
+  genres: [ 'industrial', 'industrial metal' ]
+});
 albumList.push({
-              artistName: 'Metallica',
-              name: 'Metallica',
-              releaseDate: '1991, August 12',
-              genres: [ 'heavy metal' ]
-            });
+  artistName: 'Metallica',
+  name: 'Metallica',
+  releaseDate: '1991, August 12',
+  genres: [ 'heavy metal' ]
+});
 albumList.push({
-              artistName: 'The Prodigy',
-              name: 'Music for the Jilted Generation',
-              releaseDate: '1994, July 4',
-              genres: [ 'electronica', 'breakbeat hardcore', 'rave', 'jungle' ]
-            });
+  artistName: 'The Prodigy',
+  name: 'Music for the Jilted Generation',
+  releaseDate: '1994, July 4',
+  genres: [ 'electronica', 'breakbeat hardcore', 'rave', 'jungle' ]
+});
 albumList.push({
-              artistName: 'Johnny Cash',
-              name: 'Unchained',
-              releaseDate: '1996, November 5',
-              genres: [ 'country', 'rock' ]
-            });
+  artistName: 'Johnny Cash',
+  name: 'Unchained',
+  releaseDate: '1996, November 5',
+  genres: [ 'country', 'rock' ]
+});
+
+var sampleSongs = [];
+
+sampleSongs.push({ name: 'Swamped',
+trackNumber: 1
+});
+sampleSongs.push({ name: "Heaven's a Lie",
+trackNumber: 2
+});
+sampleSongs.push({ name: 'Daylight Dancer',
+trackNumber: 3
+});
+sampleSongs.push({ name: 'Humane',
+trackNumber: 4
+});
+sampleSongs.push({ name: 'Self Deception',
+trackNumber: 5
+});
+sampleSongs.push({ name: 'Aeon',
+trackNumber: 6
+});
+sampleSongs.push({ name: 'Tight Rope',
+trackNumber: 7
+});
+
+albumList.forEach(function(album){
+  album.songs = sampleSongs;
+});
 
 
 db.Album.remove({}, function(err, albums){
